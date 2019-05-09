2aa1089de5467aff66a432841e5f9555ed15db74
Breno Henrique <brenohff@gmail.com>
2019-05-08 23:43:29 -0300

    Avançando com o curso

--- gerenciador_loja/lib/tabs/users_tab.dart
+++ gerenciador_loja/lib/tabs/users_tab.dart
@@ -1,8 +1,13 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:gerenciador_loja/blocs/user_bloc.dart';
+import 'package:gerenciador_loja/tiles/user_tile.dart';
 
 class UsersTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    UserBloc _userBloc = BlocProvider.of<UserBloc>(context);
+
     return Column(
       children: <Widget>[
         Padding(
@@ -15,19 +20,38 @@ class UsersTab extends StatelessWidget {
               icon: Icon(Icons.search, color: Colors.white),
               border: InputBorder.none,
             ),
+            onChanged: _userBloc.onChangedSearch,
           ),
         ),
         Expanded(
-          child: ListView.separated(
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text("Teste $index"),
-              );
+          child: StreamBuilder<List>(
+            stream: _userBloc.outUsers,
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return Center(
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation(Colors.pinkAccent),
+                  ),
+                );
+              } else if (snapshot.data.length == 0) {
+                return Center(
+                  child: Text(
+                    "Nenhum usuário encontrado :(",
+                    style: TextStyle(color: Colors.pinkAccent),
+                  ),
+                );
+              } else {
+                return ListView.separated(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (context, index) {
+                    return UserTile(snapshot.data[index]);
+                  },
+                  separatorBuilder: (context, index) {
+                    return Divider();
+                  },
+                );
+              }
             },
-            separatorBuilder: (context, index) {
-              return Divider();
-            },
-            itemCount: 20,
           ),
         ),
       ],
